3f8803089559a0415fb3dd790f185c54f0e6c097
jhsong <wngml203@naver.com>
2022-12-29 21:43:38 +0900

    init

---
+++ src/main/java/com/example/ddooheeJpa/DdooheeJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddooheeJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdooheeJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdooheeJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ddooheeJpa/DdooheeJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddooheeJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdooheeJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
