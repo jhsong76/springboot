b18ad48adf0942561694519589b2ac6659b145e7
jhsong <wngml203@naver.com>
2023-01-04 23:36:23 +0900

    Entity class 개발2

---
+++ src/main/java/com/example/ddooheeJpa/domain/Category.java
@@ -0,0 +1,39 @@
+package com.example.ddooheeJpa.domain;
+
+import com.example.ddooheeJpa.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    /*
+     * 내 연관관계는,
+     * 자식은 여러개를 가질 수 있기 때문에 List
+     */
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+}
--- src/main/java/com/example/ddooheeJpa/domain/item/Item.java
+++ src/main/java/com/example/ddooheeJpa/domain/item/Item.java
@@ -1,9 +1,12 @@
 package com.example.ddooheeJpa.domain.item;
 
+import com.example.ddooheeJpa.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 한 테이블에 다 때려박는것.
@@ -19,5 +22,9 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany
+    @JoinTable(name = "items") // 중간 테이블 mapping 필요
+    private List<Category> categories = new ArrayList<>();
 
 }
