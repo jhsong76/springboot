31eedd0afb7e435c2ef0c576eeb7099305dc1127
jhsong <wngml203@naver.com>
2023-01-12 13:10:49 +0900

    강의 중간에 끊음,,

--- src/main/java/com/example/ddooheeJpa/api/OrderSimpleApiController.java
+++ src/main/java/com/example/ddooheeJpa/api/OrderSimpleApiController.java
@@ -1,13 +1,20 @@
 package com.example.ddooheeJpa.api;
 
+import com.example.ddooheeJpa.domain.Address;
 import com.example.ddooheeJpa.domain.Order;
+import com.example.ddooheeJpa.domain.OrderStatus;
 import com.example.ddooheeJpa.domain.repository.OrderRepository;
 import com.example.ddooheeJpa.domain.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * XToOne(ManyToOne, OneToOne)
@@ -28,5 +35,32 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); // Lazy 강제 초기화
         }
         return all;
+    }
+
+    @GetMapping("api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
