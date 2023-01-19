ccc01a879cc61a6bb3344ae856e476a50bbbd17f
jhsong <wngml203@naver.com>
2023-01-19 23:53:33 +0900

    유저 관심사, 성격 배열 반환 성공

--- src/main/java/com/example/ddooheeJpa/match/controller/MatchController.java
+++ src/main/java/com/example/ddooheeJpa/match/controller/MatchController.java
@@ -3,12 +3,18 @@ package com.example.ddooheeJpa.match.controller;
 
 import com.example.ddooheeJpa.match.dto.*;
 import com.example.ddooheeJpa.match.service.MatchService;
+import com.example.ddooheeJpa.user.entity.User;
+import com.example.ddooheeJpa.user.entity.UserInterest;
+import com.example.ddooheeJpa.user.entity.UserPersonality;
+import com.example.ddooheeJpa.user.repository.UserRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +22,7 @@ import java.util.List;
 public class MatchController {
     private final MatchService matchService;
 
+    private final UserRepository userRepository;
     // 매칭 시도
     @PostMapping("/{userMatching}/{userGetMatched}")
     public ResponseEntity<MatchResDto> matching(@PathVariable("userMatching") long userMatching, @PathVariable("userGetMatched") long userGetMatched) {
@@ -54,9 +61,63 @@ public class MatchController {
     }
 
     // 나에게 매칭 시도한 유저 전체 조회
-    @GetMapping("/{userGetMatched}")
-    public ResponseEntity<List<UserMatchList>> GetMatchedList(@PathVariable("userGetMatched")Long userGetMatched) {
-        final List<UserMatchList> response = matchService.GetMatchedList(userGetMatched);
-        return ResponseEntity.ok().body(response);
+    @GetMapping("/AllUser")
+    public ResponseEntity<List<UserGetMatchList>> GetMatchedList() {
+        List<User> users = userRepository.findAll();
+        List<UserGetMatchList> result = users.stream()
+                .map(u -> new UserGetMatchList(u))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(result);
+    }
+
+    @Data
+    static class UserGetMatchList{
+        private Long userId;
+        private String userNickName;
+        private String userMajorName;
+        private String userStudentNum;
+        private int userGender;
+        private String userInfo;
+        private String userprofileImg;
+        private String userMbti;
+
+        private List<UserInterestDto> userInterests;
+        private List<UserPersonalityDto> userPesonalitys;
+
+        public UserGetMatchList(User user) {
+            userId = user.getUserId();
+            userNickName = user.getUserNickName();
+            userMajorName = user.getUserMajorName();
+            userStudentNum = user.getUserStudentNum();
+            userGender = user.getUserGender();
+            userInfo = user.getUserInfo();
+            userprofileImg = user.getUserProfileImg();
+            userMbti = user.getUserMbti();
+            userInterests = user.getUserInterests().stream()
+                    .map(userInterest -> new UserInterestDto(userInterest))
+                    .collect(Collectors.toList());
+            userPesonalitys = user.getUserPesonalitys().stream()
+                    .map(userPersonality -> new UserPersonalityDto(userPersonality))
+                    .collect(Collectors.toList());
+        }
+
+    }
+
+    @Data
+    static class UserInterestDto{
+        private String Interest;
+
+        public UserInterestDto(UserInterest userInterest) {
+            Interest = userInterest.getInterest();
+        }
+    }
+
+    @Data
+    static class UserPersonalityDto{
+        private String personality;
+
+        public UserPersonalityDto(UserPersonality userPersonality) {
+            personality = userPersonality.getPersonality();
+        }
     }
 }
