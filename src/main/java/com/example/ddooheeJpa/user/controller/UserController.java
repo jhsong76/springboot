989edd50df7578e9e397ff9a8f8e93651699f386
jhsong <wngml203@naver.com>
2023-02-02 00:01:43 +0900

    UserController Swagger 적용

--- src/main/java/com/example/ddooheeJpa/user/controller/UserController.java
+++ src/main/java/com/example/ddooheeJpa/user/controller/UserController.java
@@ -9,6 +9,9 @@ import com.example.ddooheeJpa.report.service.ReportService;
 import com.example.ddooheeJpa.user.dto.UserDetailDto;
 import com.example.ddooheeJpa.user.dto.UserListDto;
 import com.example.ddooheeJpa.user.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +28,16 @@ public class UserController {
     private final ReportService reportService;
     private final BlockService blockService;
 
-    // 유저 정보 상세 조회
+    /**
+     * 유저 정보 상세 조회
+     * [GET] /users/:id
+     * @return ResponseEntity<UserDetailDto>
+     */
+    @ApiOperation(value = "유저 상제 조회 API", notes = "유저 인덱스 값 입력시 해당하는 유저의 상세 정보 반환")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "api 정상 작동"),
+            @ApiResponse(code = 400, message = "해당 유저가 존재하지 않습니다.")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<UserDetailDto> findUserById(@PathVariable("id") long userId) {
 
@@ -34,7 +46,17 @@ public class UserController {
         return ResponseEntity.ok().body(response);
     }
 
-    // 유저 좋아요 등록
+    /**
+     * 유저 좋아요 등록
+     * [POST] /users/:userGiveLikes/:userGetLikes
+     * @return ResponseEntity<LikeDto>
+     */
+    @ApiOperation(value = "유저 좋아요 등록 API", notes = "userGiveLikes -> 좋아요를 한 유저, userGetLikes -> 좋아요를 받은 유저," +
+            "userGiveLikes가 userGetLikes를 좋아요 등록")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "api 정상 작동"),
+            @ApiResponse(code = 400, message = "해당 유저가 존재하지 않습니다. ")
+    })
     @PostMapping("/like/{userGiveLikes}/{userGetLikes}")
     public ResponseEntity<LikeDto> userLikes(@PathVariable("userGiveLikes") long userGiveLikes,
                                              @PathVariable("userGetLikes") long userGetLikes) {
@@ -43,7 +65,17 @@ public class UserController {
         return ResponseEntity.ok().body(response);
     }
 
-    // 유저 신고 등록
+    /**
+     * 유저 신고 등록
+     * [POST] /users/report/:userReport/:userGetReported
+     * @return ResponseEntity<ReportDto>
+     */
+    @ApiOperation(value = "유저 신고 등록 API", notes = "userReport -> 신고를 한 유저, userGetReported -> 신고를 받은 유저")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "api 정상 작동"),
+            @ApiResponse(code = 400, message = "해당 유저가 존재하지 않습니다."),
+            @ApiResponse(code = 400, message = "신고 사유를 작성해주세요.")
+    })
     @PostMapping("/report/{userReport}/{userGetReported}")
     public ResponseEntity<ReportDto> userReport(@PathVariable("userReport") long userReport,
                                                 @PathVariable("userGetReported") long userGetReported,
@@ -53,7 +85,16 @@ public class UserController {
 
     }
 
-    // 차단 리스트 조회
+    /**
+     * 차단 리스트 조회
+     * [GET] /users/block/:userGiveBlock
+     * @return ResponseEntity<List<UserListDto>>
+     */
+    @ApiOperation(value = "차단 리스트 조회 api", notes = "userGiveBlock이 차단한 유저 리스트 반환")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "api 정상 작동"),
+            @ApiResponse(code = 400, message = "예상치 못한 예외가 발생했습니다.")
+    })
     @GetMapping("/block/{userGiveBlock}")
     public ResponseEntity<List<UserListDto>> GetBlockedList(@PathVariable("userGiveBlock") long userGiveBlock) {
         List<UserListDto> response = blockService.BlockList(userGiveBlock);
